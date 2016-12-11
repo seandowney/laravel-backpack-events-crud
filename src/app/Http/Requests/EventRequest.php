92ceab90d3cb3f2047fb342f607c88ef750cc66b
Sean Downey <sean@considerweb.com>
2016-12-11 14:37:10 +0000

    Adding the EventRequest class

---
+++ src/app/Http/Requests/EventRequest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace SeanDowney\BackpackEventsCrud\app\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class EventRequest extends \Backpack\CRUD\app\Http\Requests\CrudRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // only allow updates if the user is logged in
+        return \Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|min:5|max:255'
+            'slug' => 'unique:events,slug,'.\Request::get('id'),
+            'speaker' => 'min:5|max:255'
+            'start_time' => 'required|date_format:"Y-m-d H:i:s"'
+            'body' => 'required|min:5|max:255'
+            'ticket_vendor' => 'numeric',
+            'ticket_vendor_id' => 'numeric',
+            'venue_id' => 'numeric',
+            'meta_description' => 'min:0|max:255'
+            'status' => 'required|in:0,1'
+        ];
+    }
+
+    /**
+     * Get the validation attributes that apply to the request.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            //
+        ];
+    }
+}
